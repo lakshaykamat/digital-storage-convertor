5c192463d83359727ac5a5ef0c3e18ebe522fbce
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-01 18:02:01 +0530

    little bit css added

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const inputOptions = document.getElementById('inputOptions')
 const outputOptions = document.getElementById('outputOptions')
 function conversion(){
     if(inputOptions.value == "bit" && outputOptions.value == "kb" ){
-        result.value= input.value/1024
+        result.value= (input.value/1024).toFixed(4)
+    }
+    else if(inputOptions.value == "bit" && outputOptions.value == "mb"){
+        result.value= (input.value/1.049e+6).toFixed(4)
+    }
+    else if(inputOptions.value == "bit" && outputOptions.value == "gb"){
+        result.value= (input.value/1.074e+9).toFixed(4)
+    }
+    else if(inputOptions.value == "bit" && outputOptions.value == "tb"){
+        result.value= (input.value/1.1e+12).toFixed(4)
     }
 }
 setInterval(conversion,0.1)
